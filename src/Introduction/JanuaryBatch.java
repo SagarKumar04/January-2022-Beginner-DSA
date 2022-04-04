2cdb1f2a5fd516a420aec227100c4d5cef592380
SagarKumar04 <sagarsngh93@gmail.com>
2022-04-04 22:58:27 +0530

    Introduction to OOPS

---
+++ src/Introduction/JanuaryBatch.java
@@ -0,0 +1,14 @@
+package Introduction;
+
+public class JanuaryBatch {
+    String name;
+    String email;
+    String contactNumber;
+    String batchName;
+    static int batchStrength;
+    static String progressionInchargeName;
+
+    public static void main(String[] args) {
+        JanuaryBatch shubham = new JanuaryBatch();
+    }
+}
