5a3fbd58453f66c82e9c7c67c0875d6d260285b7
SagarKumar04 <sagarsngh93@gmail.com>
2022-04-29 23:48:24 +0530

    Closest Prime

---
+++ src/AssignmentSolving/ClosestPrime.java
@@ -0,0 +1,64 @@
+package AssignmentSolving;
+
+import java.util.Scanner;
+
+public class ClosestPrime {
+    private static boolean checkIfNumberIsPrime(int n) {
+        boolean isPrime = true;
+
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        return isPrime;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        if(n == 1) {
+            System.out.println("2");
+        }
+        else {
+            boolean isNPrime = checkIfNumberIsPrime(n);
+
+            if(isNPrime) {
+                System.out.println(n);
+            }
+            else {
+                //find prime number before n
+                int primeNumberBeforeN = -1;
+
+                for(int i = n - 1; ; i--) {
+                    if(checkIfNumberIsPrime(i)) {
+                        primeNumberBeforeN = i;
+                        break;
+                    }
+                }
+
+                //find prime number after n
+                int primeNumberAfterN = -1;
+                for(int i = n + 1; ; i++) {
+                    if(checkIfNumberIsPrime(i)) {
+                        primeNumberAfterN = i;
+                        break;
+                    }
+                }
+
+                if(n - primeNumberBeforeN < primeNumberAfterN - n) {
+                    System.out.println(primeNumberBeforeN);
+                }
+                else if(n - primeNumberBeforeN > primeNumberAfterN - n) {
+                    System.out.println(primeNumberAfterN);
+                }
+                else {
+                    System.out.println(primeNumberBeforeN);
+                }
+            }
+        }
+    }
+}
